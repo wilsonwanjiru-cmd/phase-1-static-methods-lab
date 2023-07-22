1df37e2a514d312d81dd724b4d0ff48dfa9c6dfd
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-22 18:12:53 +0300

    <Static Methods Lab>

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    const nonCapitalizableWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    const words = string.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !nonCapitalizableWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
 }
+
+module.exports = Formatter;
